216164c99cd70e0f825c87b97eb6d027e9d58790
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-18 18:33:20 +0100

    Load input data as a 2d tensor

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,25 @@
+let data;
+
+function preload() {
+    data = loadJSON('../data/colorData.json');
+}
+
 function setup() {
     createCanvas(400, 400);
+    // console.log(data.entries.length);
+
+    let colors = [];
+
+    for (let record of data.entries) {
+        let col = [record.r / 255, record.g / 255, record.b / 255]; // normalize color values
+        colors.push(col);
+    }
+    // console.log(colors);
+
+    // load input data as a 2d tensor
+    let xs = tf.tensor2d(colors);
+    // console.log(xs.shape);
+
 }
 
 function draw() {
