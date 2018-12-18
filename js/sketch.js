4c7fd814e69f89677f4a2703687412786c73b3df
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-18 18:49:53 +0100

    Load target outputs as one-hot tensor

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,17 @@
 let data;
+let xs, ys;
+
+let labelList = [
+    'red-ish',
+    'green-ish',
+    'blue-ish',
+    'orange-ish',
+    'yellow-ish',
+    'pink-ish',
+    'purple-ish',
+    'brown-ish',
+    'grey-ish'
+];
 
 function preload() {
     data = loadJSON('../data/colorData.json');
@@ -9,17 +22,29 @@ function setup() {
     // console.log(data.entries.length);
 
     let colors = [];
+    let labels = [];
 
     for (let record of data.entries) {
         let col = [record.r / 255, record.g / 255, record.b / 255]; // normalize color values
         colors.push(col);
+        labels.push(labelList.indexOf(record.label));
     }
     // console.log(colors);
+    // console.log(labels);
 
     // load input data as a 2d tensor
-    let xs = tf.tensor2d(colors);
+    xs = tf.tensor2d(colors);
     // console.log(xs.shape);
 
+    // load target outputs data as a 2d tensor
+    let labelsTensor = tf.tensor1d(labels, 'int32');
+    // labelsTensor.print();
+
+    // create one-hot tensor
+    ys = tf.oneHot(labelsTensor, 9);
+
+    // xs.print();
+    // ys.print();
 }
 
 function draw() {
